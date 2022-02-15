a43f804d8259e314be35d86e78785c29b3d164a4
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2022-02-15 17:14:29 +0330

    set up components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './layout/components/home/home.component';
 import { LayoutModule } from './layout/layout.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { MainModule } from './main/main.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule],
+  imports: [
+    MainModule,
+    LayoutModule,
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormlyBootstrapModule,
+    FormlyModule.forRoot({ extras: { lazyRender: true } }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
