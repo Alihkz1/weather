90fae70b60416e37a19e21fc839dc751321335be
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2022-02-16 10:37:49 +0330

    fix : main module

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,42 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './components/home/home.component';
 import { InputComponent } from './components/input/input.component';
 import { HttpClientModule } from '@angular/common/http';
 import { OutPutComponent } from './components/out-put/out-put.component';
 import { OutPutFacade } from './components/out-put/out-put.facade';
-import { RouterModule, Routes } from '@angular/router';
 import { MaterialModule } from 'src/shared/modules/material/material.module';
 import { FormModule } from 'src/shared/modules/formly/formly.module';
-const routes: Routes = [
-  {
-    path: '',
-    component: MainComponent,
-  },
-  {
-    path: 'inPut',
-    component: InputComponent,
-  },
-  {
-    path: 'outPut',
-    component: OutPutComponent,
-  },
-  {
-    path: '**',
-    component: MainComponent,
-  },
-];
 @NgModule({
-  declarations: [MainComponent, InputComponent, OutPutComponent],
-  imports: [
-    CommonModule,
-    FormModule,
-    MaterialModule,
-    HttpClientModule,
-
-    RouterModule.forChild(routes),
-  ],
-  exports: [MainComponent],
+  declarations: [HomeComponent, InputComponent, OutPutComponent],
+  imports: [CommonModule, FormModule, MaterialModule, HttpClientModule],
+  exports: [HomeComponent],
   providers: [OutPutFacade],
 })
 export class MainModule {}
