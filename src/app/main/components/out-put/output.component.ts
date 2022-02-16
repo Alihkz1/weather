3a1b4d21d0a1e0ca60d3d2e1512da1e1a60acb4a
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2022-02-16 12:53:46 +0330

    faet : dialog : style

--- src/app/main/components/out-put/output.component.ts
+++ src/app/main/components/out-put/output.component.ts
@@ -27,7 +27,7 @@ export class OutPutComponent implements OnInit {
   }
 
   public onTryAgain() {
-    this.dialog.open(InputComponent, { height: '30%', width: '80%' });
+    this.dialog.open(InputComponent, { height: '30%', width: '40%' });
   }
 
   private onConvertInfo(info: any) {
