98d372140a6863cf57f51f96707f927af95f2b68
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2022-02-16 11:10:28 +0330

    refactor : out-put.ts

--- src/app/main/components/out-put/out-put.component.ts
+++ src/app/main/components/out-put/out-put.component.ts
@@ -17,18 +17,34 @@ export class OutPutComponent implements OnInit {
   constructor(private outPutFacade: OutPutFacade, private router: Router) {}
 
   ngOnInit(): void {
-    this.outPutFacade.cityInfo.subscribe((info) => {
-      if (info.length == 0) this.router.navigate(['/main']);
-      this.name = info.name;
-      this.feelsLike = Number((info.feelsLike - 272.25)?.toFixed(0));
-      this.humidity = Number(info.humidity?.toFixed(0));
-      this.temp = Number((info.temp - 272.25)?.toFixed(0));
-      this.max = Number((info.temp_max - 272.25)?.toFixed(0));
-      this.min = Number((info.temp_min - 272.25)?.toFixed(0));
-    });
+    this.onGetCityInfo();
   }
 
   public onTryAgain() {
     this.router.navigate(['/main']);
+  }
+
+  private onConvertInfo(info: any) {
+    info = {
+      ...info,
+      feelsLike: Number((info?.feelsLike - 272.25)?.toFixed(0)),
+      temp: Number((info?.temp - 272.25)?.toFixed(0)),
+      max: Number((info?.temp_max - 272.25)?.toFixed(0)),
+      min: Number((info?.temp_min - 272.25)?.toFixed(0)),
+    };
+    return info;
+  }
+
+  private onGetCityInfo() {
+    this.outPutFacade.cityInfo.subscribe((info) => {
+      if (info.length == 0) this.router.navigate(['/main']);
+      info = this.onConvertInfo(info);
+      this.name = info?.name;
+      this.feelsLike = info?.feelsLike;
+      this.humidity = info?.humidity;
+      this.temp = info?.temp;
+      this.max = info?.max;
+      this.min = info?.min;
+    });
   }
 }
